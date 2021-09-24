6554f972d265e96c72f9010eb385766407a81c20
Pool Ortega <poolortegas@gmail.com>
2021-09-24 15:20:02 -0500

    data2

--- config/db.js
+++ config/db.js
@@ -1,20 +1,17 @@
-//Requiero mongoose
-
 const mongoose = require('mongoose')
-
 const connectingDB = async () => {
+  // ERROR HANDLING - try catch - node.js
   try {
-    //Nos conectamos al servidor de mongo por la variable de entorno
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
-    return console.log('Conectados a la base de datos!')
+
+    return console.log('Conectados a la base de datos')
   } catch (error) {
     console.log(error)
-    //cierra el server
     process.exit(1)
   }
 }
-//Exportamos la conexion al servidor
+
 module.exports = connectingDB
